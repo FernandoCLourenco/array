730020caca758ed5ecde7a68c8e2aee0c008c9db
Fernando Carosi Lourenço <zippetanimations@gmail.com>
2025-04-14 20:08:56 -0300

    ,

--- apps.js
+++ apps.js
@@ -7,10 +7,10 @@ usuarioS.push("CelsoPorta78")
 
 console.dir(usuarioS)
 
-document.getElementById("conteudo").innerHTML = usuarioS[0] + "-"
-document.getElementById("conteudo").innerHTML = usuarioS[1] + "-"
-document.getElementById("conteudo").innerHTML = usuarioS[2] + "-"
-document.getElementById("conteudo").innerHTML = usuarioS[3] + "-"
-document.getElementById("conteudo").innerHTML = usuarioS[4] + "-"
-document.getElementById("conteudo").innerHTML = usuarioS[5] + "-"
+document.getElementById("conteudo").innerHTML += usuarioS[0] + "-"
+document.getElementById("conteudo").innerHTML += usuarioS[1] + "-"
+document.getElementById("conteudo").innerHTML += usuarioS[2] + "-"
+document.getElementById("conteudo").innerHTML += usuarioS[3] + "-"
+document.getElementById("conteudo").innerHTML += usuarioS[4] + "-"
+document.getElementById("conteudo").innerHTML += usuarioS[5] + "-"
 
