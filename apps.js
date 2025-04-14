b5d9e853c64a969bee0850bba41d863e3fc45bf2
Fernando Carosi Lourenço <zippetanimations@gmail.com>
2025-04-14 20:08:32 -0300

    .

---
+++ apps.js
@@ -0,0 +1,16 @@
+let usuarioS = new Array()
+usuarioS.push("Cleitin2212")
+usuarioS.push("Vagnerinfo")
+usuarioS.push("AnaCarolina221")
+usuarioS.push("MatiasGamer")
+usuarioS.push("CelsoPorta78")
+
+console.dir(usuarioS)
+
+document.getElementById("conteudo").innerHTML = usuarioS[0] + "-"
+document.getElementById("conteudo").innerHTML = usuarioS[1] + "-"
+document.getElementById("conteudo").innerHTML = usuarioS[2] + "-"
+document.getElementById("conteudo").innerHTML = usuarioS[3] + "-"
+document.getElementById("conteudo").innerHTML = usuarioS[4] + "-"
+document.getElementById("conteudo").innerHTML = usuarioS[5] + "-"
+
